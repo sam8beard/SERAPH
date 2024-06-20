91c27c53802410c33472844679b68d1d126f0b29
Sammy Beard <91227894+sam8beard@users.noreply.github.com>
2024-06-20 08:33:05 -0400

    Setting up virtual environment

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__) 
 CORS(app) # allows us to send cross origin requests to our app 
  
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://localhost:110402@localhost:5432/postgres'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:110402@localhost:5432/postgres'
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 
