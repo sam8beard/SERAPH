79e8d721555ace50ae0c1879ee62192faf01fa23
lauren.perkins114 <lauren.perkins114@gmail.com>
2024-06-20 10:24:38 -0400

    Home Page with React and JSX (Lauren and Skyler)

---
+++ frontend/src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+export const SignIn = () => {
+  return (
+    <div className="sign-in">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <img className="image" alt="Image" src="image-1.png" />
+          <img className="rectangle" alt="Rectangle" src="rectangle-11.png" />
+          <p className="text-wrapper">S E R A P H</p>
+          <div className="div" />
+          <div className="rectangle-2" />
+          <div className="text-wrapper-2">Username</div>
+          <div className="text-wrapper-3">Password</div>
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+          <div className="text-wrapper-4">Login</div>
+          <div className="text-wrapper-5">CAC SSO</div>
+          <img className="img" alt="Image" src="image-4.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
