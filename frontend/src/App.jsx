6babc173490a1a9f2ee9296fdaaaac0074ccd4ea
Sammy Beard <91227894+sam8beard@users.noreply.github.com>
2024-06-25 10:09:36 -0400

    getting things sorted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,33 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard.jsx'
+import NavigationBar from './components/NavigationBar.jsx'
+import Testing from './pages/Testing.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <Router>
+        <div className="nav-container"> 
+          <NavigationBar />
+        </div>
+        
+        <Routes>
+          <Route 
+              path='/'
+              element={<Dashboard/>}
+          /> 
+          <Route 
+              path='/testing'
+              element={<Testing />}
+          />
+        </Routes>
+      </Router>
+    </div>
+  </> 
   )
 }
 
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './Dashboard.css';
+
+function Dashboard() { 
+    return (
+        <div className='parent'> 
+            <h1> Hello </h1>
+
+        </div>
+    )
+}
+
+export default Dashboard
