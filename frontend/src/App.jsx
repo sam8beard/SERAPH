3a6d5c8be4dc47f4b78685629609c7a68de15a2b
RayX52 <127812971+RayX52@users.noreply.github.com>
2024-06-25 10:08:35 -0400

    added project view page html and css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ProjectView from './pages/ProjectView.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ProjectView />
   </React.StrictMode>,
 )
