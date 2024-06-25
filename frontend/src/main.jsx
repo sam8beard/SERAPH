73209658cb7542d17713a0906e5e5669fc49c0bb
lauren.perkins114 <lauren.perkins114@gmail.com>
2024-06-25 10:06:21 -0400

    Update Beck Code (After it was deleted)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SignInPage from './SignInPage.jsx'
+import ProjectArchivedPage from './ProjectArchivedPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SignInPage />
+    <ProjectArchivedPage />
   </React.StrictMode>,
 )
