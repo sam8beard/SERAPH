81499d9411472acecc42e46432f1766816fa4eff
dw05164 <dw05164@uga.edu>
2024-06-25 07:59:36 -0400

    pdf overview

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './pages/PDFOverview.jsx'
+import PDFOverview from './pages/PDFOverview.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PDFOverview />
   </React.StrictMode>,
 )
---
+++ frontend/src/pages/PDFOverview.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./PDFOverview.css";
+
+export const PDFOverview = () => {
+  return (
+    <div className="export-PDF-page">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="div" />
+          <img className="image" alt="Image" src="image-5.png" />
+          <p className="text-wrapper">S E R A P H</p>
+          <div className="text-wrapper-2">Your Project PDF</div>
+          <div className="text-wrapper-3">Welcome User</div>
+          <div className="rectangle-2" />
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+          <div className="rectangle-5" />
+          <div className="rectangle-6" />
+          <div className="rectangle-7" />
+          <div className="rectangle-8" />
+          <div className="rectangle-9" />
+          <div className="rectangle-10" />
+          <div className="rectangle-11" />
+          <div className="rectangle-12" />
+          <div className="rectangle-13" />
+          <div className="rectangle-14" />
+          <div className="rectangle-15" />
+          <div className="rectangle-16" />
+          <div className="rectangle-17" />
+          <div className="rectangle-18" />
+          <img className="line" alt="Line" src="line-38.svg" />
+          <img className="img" alt="Line" src="line-39.svg" />
+          <img className="line-2" alt="Line" src="line-40.svg" />
+          <img className="line-3" alt="Line" src="line-41.svg" />
+          <img className="line-4" alt="Line" src="line-42.svg" />
+          <img className="line-5" alt="Line" src="line-43.svg" />
+          <img className="line-6" alt="Line" src="line-44.svg" />
+          <img className="line-7" alt="Line" src="line-45.svg" />
+          <div className="text-wrapper-4">MEDUSA</div>
+          <div className="text-wrapper-5">Metrics</div>
+          <div className="text-wrapper-6">Sprint 1</div>
+          <div className="text-wrapper-7">Sprint 2</div>
+          <div className="text-wrapper-8">Sprint 3</div>
+          <img src="../assets/SERAPH_ICON.png" />
+          <div className="group">
+            <div className="overlap-group">
+                <div className="buttons">
+                    <button className="save-button">Save PDF</button>
+                    <button className="cancel-button">Cancel</button>
+                </div>
+            </div>
+          </div>
+          
+          
+          <div className="text-wrapper-10">Medusa.pdf</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PDFOverview;
+ 
+
+
+/* import React from "react";
+import "./PDFOverview.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function PDFOverview() {
+	return (
+       
+		<div className="background">
+            <div className="logo">
+            </div>
+            <div className="title">
+                <h1>GeeksForGeeksafdsfasdfasdfadsf</h1>
+            </div>
+		</div>
+	);
+}
+
+export default PDFOverview; */
