b411218bee6460537f161c0696027b910f5cf459
Sammy Beard <91227894+sam8beard@users.noreply.github.com>
2024-06-26 15:42:18 -0400

    adding react hooks and displaying content

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './Dashboard.css';
+import { useState, useEffect } from 'react'
+import ProjectCard from '../components/dashboard_components/ProjectCard';
 
 function Dashboard() { 
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        fetchProjects()
+    }, [])
+    const fetchProjects = async () => { 
+        const response = await fetch("http://127.0.0.1:5000/get_projects")
+        const data = await response.json()
+        setProjects(data.projects)
+        console.log(data.projects)
+    }
     return (
         <div className='parent'> 
-            <h1> Hello </h1>
-
+            <ProjectCard projects={projects}/> 
         </div>
     )
 }
