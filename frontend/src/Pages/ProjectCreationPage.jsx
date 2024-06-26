4403aff328a6491f105384e03544125d0ec24c6d
lauren.perkins114 <lauren.perkins114@gmail.com>
2024-06-26 08:43:22 -0400

    Inline JSX for my Pages

--- frontend/src/Pages/ProjectCreationPage.jsx
+++ frontend/src/Pages/ProjectCreationPage.jsx
@@ -1,58 +1,290 @@
-import React from "react";
-import './projectcreationstyle.css'; 
+import React from 'react';
+import plussign from '../assets/Plus_Sign.png';
 
-import plussign from './assets/Plus_Sign.png';
-import seraph_logo from './assets/seraph_logo.png';
+function ProjectCreation() {
 
-function ProjectCreationPage() {
-    return (
-            <div className="project-creation">
-                <div className="div">
-                    {/*Div for the Nav Bar*/}
-                    <div className="div-2">
-                        <div className="text-wrapper-1">Welcome User</div>
-                        <div className="p">S E R A P H</div>
-                        <img className="image" alt="Image" src={seraph_logo} />
-			            <div className="text-wrapper-2">Project Creation</div>
-                    </div>
+  const styles = {
+    '*': {
+      margin: 0,
+      padding: 0,
+    },
+    projectCreationPC: {
+      backgroundColor: '#ffffff',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      width: '100%',
+    },
+    divPC: {
+      backgroundColor: '#ffffff',
+      height: '1080px',
+      width: '1920px',
+      overflow: 'hidden',
+    },
+    div2PC: {
+      backgroundColor: '#20375d',
+      height: '128px',
+      left: 0,
+      right: 0,
+      position: 'absolute',
+      top: 0,
+      width: '1924px',
+    },
+    pPC: {
+      color: '#d9d9d9',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '48px',
+      fontWeight: 400,
+      height: '60px',
+      left: '143px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      top: '34px',
+      width: '264px',
+    },
+    imagePC: {
+      height: '87px',
+      left: '33px',
+      objectFit: 'cover',
+      position: 'absolute',
+      top: '18px',
+      width: '86px',
+    },
+    textWrapper1PC: {
+      color: '#fffdfd',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '20px',
+      fontWeight: 400,
+      left: '1684px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      textAlign: 'center',
+      top: '51px',
+      width: '202px',
+    },
+    textWrapper2PC: {
+      color: '#fffdfd',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '48px',
+      fontWeight: 400,
+      left: '772px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      top: '32px',
+      width: '376px',
+    },
+    div3PC: {
+      height: '952px',
+      top: '128px',
+      position: 'absolute',
+      width: '1920px',
+    },
+    textWrapper3PC: {
+      color: '#000000',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '20px',
+      fontWeight: 400,
+      left: '33px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      zIndex: 2,
+      top: '46px',
+    },
+    textWrapper4PC: {
+      color: '#000000',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '20px',
+      fontWeight: 400,
+      left: '33px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      zIndex: 2,
+      top: '178px',
+    },
+    textWrapper5PC: {
+      color: '#000000',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '20px',
+      fontWeight: 400,
+      left: '33px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      zIndex: 2,
+      top: '310px',
+    },
+    textWrapper6PC: {
+      color: '#000000',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '20px',
+      fontWeight: 400,
+      left: '33px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      zIndex: 2,
+      top: '442px',
+    },
+    textWrapper7PC: {
+      color: '#000000',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '20px',
+      fontWeight: 400,
+      left: '33px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      zIndex: 2,
+      top: '574px',
+    },
+    textWrapper8PC: {
+      color: '#000000',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '20px',
+      fontWeight: 400,
+      left: '33px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      zIndex: 2,
+      top: '706px',
+    },
+    textWrapper9PC: {
+      color: '#000000',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '20px',
+      fontWeight: 400,
+      left: '1215px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      zIndex: 2,
+      top: '838px',
+    },
+    rectangle1PC: {
+      backgroundColor: '#d9d9d9',
+      borderRadius: '10px',
+      height: '48px',
+      left: '33px',
+      position: 'absolute',
+      zIndex: 1,
+      width: '443px',
+      top: '84px',
+    },
+    rectangle2PC: {
+      backgroundColor: '#d9d9d9',
+      borderRadius: '10px',
+      height: '48px',
+      left: '33px',
+      position: 'absolute',
+      zIndex: 1,
+      width: '237px',
+      top: '216px',
+    },
+    rectangle3PC: {
+      backgroundColor: '#d9d9d9',
+      borderRadius: '10px',
+      height: '48px',
+      left: '33px',
+      position: 'absolute',
+      zIndex: 1,
+      width: '443px',
+      top: '348px',
+    },
+    rectangle4PC: {
+      backgroundColor: '#d9d9d9',
+      borderRadius: '10px',
+      height: '48px',
+      left: '33px',
+      position: 'absolute',
+      zIndex: 1,
+      width: '237px',
+      top: '480px',
+    },
+    rectangle5PC: {
+      backgroundColor: '#d9d9d9',
+      borderRadius: '10px',
+      height: '48px',
+      left: '33px',
+      position: 'absolute',
+      zIndex: 1,
+      width: '237px',
+      top: '612px',
+    },
+    rectangle6PC: {
+      backgroundColor: '#d9d9d9',
+      borderRadius: '10px',
+      height: '48px',
+      left: '33px',
+      position: 'absolute',
+      zIndex: 1,
+      width: '443px',
+      top: '744px',
+    },
+    rectangle7PC: {
+      backgroundColor: '#d9d9d9',
+      borderRadius: '10px',
+      height: '48px',
+      left: '1156px',
+      position: 'absolute',
+      zIndex: 1,
+      width: '186px',
+      top: '825px',
+    },
+    plusSign1PC: {
+      left: '1095px',
+      position: 'absolute',
+      top: '221px',
+    },
+    plusSign2PC: {
+      left: '1095px',
+      position: 'absolute',
+      top: '480px',
+    },
+  };
 
-                    {/* */}
-                    <div className="div-3">
-                        <div className="text-wrapper-3">Title</div>
-                        <div className="rectangle-1" />
-                        <div className="text-wrapper-4">Team Members</div>
-                        
-                        <div className="rectangle-2" style={{ left: '33px' }} />
-                        <div className="rectangle-2" style={{ left: '294px' }} />
-                        <div className="rectangle-2" style={{ left: '555px' }} />
-                        <div className="rectangle-2" style={{ left: '816px' }} />
+  return (
+    <div style={styles.projectCreationPC}>
+      <div style={styles.divPC}>
+        <div style={styles.div3PC}>
+          <div style={styles.textWrapper3PC}>Title</div>
+          <div style={styles.rectangle1PC} />
+          <div style={styles.textWrapper4PC}>Team Members</div>
 
+          <div style={{ ...styles.rectangle2PC, left: '33px' }} />
+          <div style={{ ...styles.rectangle2PC, left: '294px' }} />
+          <div style={{ ...styles.rectangle2PC, left: '555px' }} />
+          <div style={{ ...styles.rectangle2PC, left: '816px' }} />
 
-                        <div className="text-wrapper-5">Customer</div>
-                        <div className="rectangle-3" />
-                        <div className="text-wrapper-6">Technologies Used</div>
-                        
-                        <div className="rectangle-4" style={{ left: '33px' }} />
-                        <div className="rectangle-4" style={{ left: '294px' }} />
-                        <div className="rectangle-4" style={{ left: '555px' }} />
-                        <div className="rectangle-4" style={{ left: '816px' }} />
+          <div style={styles.textWrapper5PC}>Customer</div>
+          <div style={styles.rectangle3PC} />
+          <div style={styles.textWrapper6PC}>Technologies Used</div>
 
-                        <div className="text-wrapper-7">Training Requests</div>
-                        <div className="rectangle-5" />
-                        <div className="text-wrapper-8">Element Chief</div>
-                        <div className="rectangle-6" />
+          <div style={{ ...styles.rectangle4PC, left: '33px' }} />
+          <div style={{ ...styles.rectangle4PC, left: '294px' }} />
+          <div style={{ ...styles.rectangle4PC, left: '555px' }} />
+          <div style={{ ...styles.rectangle4PC, left: '816px' }} />
 
-                        <div className="text-wrapper-9">Create</div>
-                        <div className="rectangle-7" />
+          <div style={styles.textWrapper7PC}>Training Requests</div>
+          <div style={styles.rectangle5PC} />
+          <div style={styles.textWrapper8PC}>Element Chief</div>
+          <div style={styles.rectangle6PC} />
 
-                        <img className="plus-sign-1" alt="Group" src={plussign} />
-                        <img className="plus-sign-2" alt="Group" src={plussign} />
+          <div style={styles.textWrapper9PC}>Create</div>
+          <div style={styles.rectangle7PC} />
 
-                    </div>
+          <img style={styles.plusSign1PC} alt="Group" src={plussign} />
+          <img style={styles.plusSign2PC} alt="Group" src={plussign} />
+        </div>
 
-                </div>
-            </div>
-    ); 
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCreationPage;
--- frontend/src/Pages/SignInPage.jsx
+++ frontend/src/Pages/SignInPage.jsx
@@ -1,31 +1,197 @@
 import React from 'react';
-import './signinstyle.css'; 
-
-import seraph_logo from './assets/seraph_logo.png';
-import dod_logo from './assets/DOD_Logo.png';
+import seraph_logo_SI from '../assets/seraph_logo.png';
+import dod_logo_SI from '../assets/DOD_Logo.png';
 
 function SignInPage() {
 
+  const styles = {
+    '*': {
+      margin: 0,
+      padding: 0,
+    },
+    signInSI: {
+      backgroundColor: '#20375d',
+      height: '1080px',
+      width: '1920px',
+      overflow: 'hidden',
+      position: 'relative',
+    },
+    overlapGroupWrapperSI: {
+      height: '100%',
+      width: '100%',
+      margin: '10px',
+    },
+    overlapGroupSI: {
+      height: '100%',
+      width: '100%',
+    },
+    textWrapperSI: {
+      color: '#ffffff',
+      fontFamily: 'Lexend-Regular, Helvetica',
+      fontSize: '150px',
+      fontWeight: 400,
+      left: '100px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      textAlign: 'center',
+      textShadow: '15px 15px 10px rgba(0, 0, 0, 0.25)',
+      whiteSpace: 'nowrap',
+      width: '840px',
+      top: '453px',
+    },
+    textWrapper2SI: {
+      color: '#000000',
+      fontFamily: 'Lexend-Light, Helvetica',
+      fontSize: '48px',
+      fontWeight: 300,
+      height: '60px',
+      left: '1132px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      zIndex: 3,
+      top: '471px',
+    },
+    textWrapper3SI: {
+      color: '#000000',
+      fontFamily: 'Lexend-Light, Helvetica',
+      fontSize: '48px',
+      fontWeight: 300,
+      height: '60px',
+      left: '1132px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      zIndex: 3,
+      top: '616px',
+    },
+    textWrapper4SI: {
+      color: '#000000',
+      fontFamily: 'Lexend-Light, Helvetica',
+      fontSize: '48px',
+      fontWeight: 300,
+      height: '60px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      textAlign: 'center',
+      zIndex: 3,
+      left: '291px',
+      top: '675px',
+    },
+    textWrapper5SI: {
+      color: '#000000',
+      fontFamily: 'Lexend-Light, Helvetica',
+      fontSize: '48px',
+      fontWeight: 300,
+      height: '60px',
+      letterSpacing: 0,
+      lineHeight: 'normal',
+      position: 'absolute',
+      textAlign: 'center',
+      zIndex: 3,
+      left: '336px',
+      top: '771px',
+    },
+    rectangle1SI: {
+      backgroundColor: '#ffffff',
+      top: '95px',
+      bottom: '139px',
+      position: 'absolute',
+      height: '898px',
+      right: '105px',
+      width: '790px',
+      opacity: 0.8,
+      borderRadius: '30px',
+      zIndex: 1,
+    },
+    rectangle2SI: {
+      position: 'absolute',
+      borderRadius: '30px',
+      zIndex: 2,
+      backgroundColor: '#D9D9D9',
+      filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+      left: '67px',
+      right: '66px',
+      top: '356px',
+      height: '76px',
+    },
+    rectangle3SI: {
+      position: 'absolute',
+      borderRadius: '30px',
+      zIndex: 2,
+      backgroundColor: '#D9D9D9',
+      filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+      left: '67px',
+      right: '66px',
+      top: '501px',
+      height: '76px',
+    },
+    rectangle4SI: {
+      position: 'absolute',
+      borderRadius: '30px',
+      zIndex: 2,
+      backgroundColor: '#D9D9D9',
+      filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+      width: '288px',
+      height: '76px',
+      left: '260px',
+      right: '261px',
+      top: '662px',
+    },
+    rectangle5SI: {
+      position: 'absolute',
+      borderRadius: '30px',
+      zIndex: 2,
+      backgroundColor: '#D9D9D9',
+      filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+      width: '288px',
+      height: '76px',
+      left: '260px',
+      right: '261px',
+      top: '761px',
+    },
+    dodLogoSI: {
+      height: '254px',
+      left: '209px',
+      position: 'absolute',
+      top: '51px',
+      width: '373px',
+      zIndex: 3,
+    },
+    seraphLogoSI: {
+      width: '1378px',
+      height: '1258px',
+      overflow: 'hidden',
+      top: '-89px',
+      left: '-159px',
+      bottom: '-89px',
+      position: 'fixed',
+      opacity: 0.25,
+    },
+  };
+
   return (
-    <div className="sign-in">
-      <div className="overlap-group-wrapper">
-        <div className="overlap-group">
-          <img className="seraph_logo" alt="Seraph Logo" src={seraph_logo} />
-          <p className="text-wrapper">S E R A P H</p>
+    <div style={styles.signInSI}>
+      <div style={styles.overlapGroupWrapperSI}>
+        <div style={styles.overlapGroupSI}>
+          <img className="seraph_logo-SI" alt="Seraph Logo" src={seraph_logo_SI} style={styles.seraphLogoSI} />
+          <p className="text-wrapper-SI" style={styles.textWrapperSI}>S E R A P H</p>
           
-          <div className="text-wrapper-2">Username</div>
-          <div className="text-wrapper-3">Password</div>
+          <div className="text-wrapper-2-SI" style={styles.textWrapper2SI}>Username</div>
+          <div className="text-wrapper-3-SI" style={styles.textWrapper3SI}>Password</div>
 
-          <div className="rectangle-1" >
-            <div className="rectangle-2" />
-            <div className="rectangle-3" />
+          <div className="rectangle-1-SI" style={styles.rectangle1SI}>
+            <div className="rectangle-2-SI" style={styles.rectangle2SI} />
+            <div className="rectangle-3-SI" style={styles.rectangle3SI} />
       
-            <div className="rectangle-4" />
-            <div className="rectangle-5" />
+            <div className="rectangle-4-SI" style={styles.rectangle4SI} />
+            <div className="rectangle-5-SI" style={styles.rectangle5SI} />
 
-            <div className="text-wrapper-4">CAC SSO</div>
-            <div className="text-wrapper-5">Login</div>
-            <img className="dod_logo" alt="DOD Logo" src={dod_logo}/>
+            <div className="text-wrapper-4-SI" style={styles.textWrapper4SI}>CAC SSO</div>
+            <div className="text-wrapper-5-SI" style={styles.textWrapper5SI}>Login</div>
+            <img className="dod_logo-SI" alt="DOD Logo" src={dod_logo_SI} style={styles.dodLogoSI} />
           </div>
         </div>
       </div>
