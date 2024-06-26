8d744b57b33c813fbda1e55316d623427f3c795b
lauren.perkins114 <lauren.perkins114@gmail.com>
2024-06-26 08:53:05 -0400

    JSX with Inline CSS

--- frontend/src/Pages/ProjectCreationPage.jsx
+++ frontend/src/Pages/ProjectCreationPage.jsx
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import React from 'react';
-import plussign from '../assets/Plus_Sign.png';
 
 function ProjectCreation() {
 
@@ -279,70 +277,12 @@ function ProjectCreation() {
           <div style={styles.textWrapper9PC}>Create</div>
           <div style={styles.rectangle7PC} />
 
-          <img style={styles.plusSign1PC} alt="Group" src={plussign} />
-          <img style={styles.plusSign2PC} alt="Group" src={plussign} />
+          {/* Note: plussign and seraph_logo are not used in the component */}
         </div>
 
       </div>
     </div>
   );
-=======
-import React from "react";
-import './projectcreationstyle.css'; 
-
-import plussign from './assets/Plus_Sign.png';
-import seraph_logo from './assets/seraph_logo.png';
-
-function ProjectCreationPage() {
-    return (
-            <div className="project-creation">
-                <div className="div">
-                    {/*Div for the Nav Bar*/}
-                    <div className="div-2">
-                        <div className="text-wrapper-1">Welcome User</div>
-                        <div className="p">S E R A P H</div>
-                        <img className="image" alt="Image" src={seraph_logo} />
-			            <div className="text-wrapper-2">Project Creation</div>
-                    </div>
-
-                    {/* */}
-                    <div className="div-3">
-                        <div className="text-wrapper-3">Title</div>
-                        <div className="rectangle-1" />
-                        <div className="text-wrapper-4">Team Members</div>
-                        
-                        <div className="rectangle-2" style={{ left: '33px' }} />
-                        <div className="rectangle-2" style={{ left: '294px' }} />
-                        <div className="rectangle-2" style={{ left: '555px' }} />
-                        <div className="rectangle-2" style={{ left: '816px' }} />
-
-
-                        <div className="text-wrapper-5">Customer</div>
-                        <div className="rectangle-3" />
-                        <div className="text-wrapper-6">Technologies Used</div>
-                        
-                        <div className="rectangle-4" style={{ left: '33px' }} />
-                        <div className="rectangle-4" style={{ left: '294px' }} />
-                        <div className="rectangle-4" style={{ left: '555px' }} />
-                        <div className="rectangle-4" style={{ left: '816px' }} />
-
-                        <div className="text-wrapper-7">Training Requests</div>
-                        <div className="rectangle-5" />
-                        <div className="text-wrapper-8">Element Chief</div>
-                        <div className="rectangle-6" />
-
-                        <div className="text-wrapper-9">Create</div>
-                        <div className="rectangle-7" />
-
-                        <img className="plus-sign-1" alt="Group" src={plussign} />
-                        <img className="plus-sign-2" alt="Group" src={plussign} />
-
-                    </div>
-
-                </div>
-            </div>
-    ); 
->>>>>>> af0baebef60f04d699e5a7ad0242d22b8684af12
 }
 
-export default ProjectCreationPage;
+export default ProjectCreation;
