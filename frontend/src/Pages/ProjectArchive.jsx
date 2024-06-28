7c891399c42c2253c517dbd001ae098b3ceb7780
bcb403 <beckbernstein27@gmail.com>
2024-06-28 13:32:09 -0400

    added button function

--- frontend/src/Pages/ProjectArchive.jsx
+++ frontend/src/Pages/ProjectArchive.jsx
@@ -1,52 +1,294 @@
 import React from "react";
-import './ProjectArchive.css'; 
-
 import seraph_logo from '../assets/seraph_logo-1.png';
 import filter_icon from '../assets/filter_icon.png';
 import search_icon from '../assets/search_icon.png';
 
-
 function ProjectArchive() {
+
+    const styles = {
+        mainContainer: {
+            backgroundColor: '#20375d',
+            height: '1080px',
+            width: '1920px',
+            overflow: 'hidden',
+            position: 'relative'
+        },
+        innerContainer: {
+            backgroundColor: '#ffffff',
+            height: '1080px',
+            width: '1920px',
+            overflow: 'hidden'
+        },
+        header: {
+            backgroundColor: '#20375d',
+            height: '128px',
+            left: 0,
+            right: 0,
+            position: 'absolute',
+            top: 0,
+            width: '1924px',
+            borderBottom: '3px solid #ffffff',
+            zIndex: 1
+        },
+        projectListContainer: {
+            backgroundColor: '#20375d',
+            height: '952px',
+            top: '75px',
+            position: 'relative',
+            width: '1920px'
+        },
+        sectionTitle: {
+            color: '#ffffff',
+            fontFamily: 'Lexend-Light, Helvetica',
+            fontSize: '30px',
+            fontWeight: 300,
+            left: '872px',
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            top: '43px',
+            width: '265px'
+        },
+        projectImage: {
+            width: '972px',
+            height: '986px',
+            right: '-249px',
+            overflow: 'hidden',
+            position: 'absolute',
+            opacity: 0.25,
+            zIndex: 1
+        },
+        exportButton: {
+            backgroundColor: '#d9d9d9',
+            position: 'absolute',
+            top: '459px',
+            width: '259px',
+            height: '69px',
+            right: '141px',
+            zIndex: 2,
+            opacity: 1,
+            borderRadius: '10px',
+            fontSize: '20px',
+        },
+        searchContainer: {
+            backgroundColor: '#dfdfdf',
+            width: '1194px',
+            height: '45px',
+            borderRadius: '10px',
+            left: '105px',
+            top: '141px',
+            position: 'absolute',
+            paddingLeft: '50px'
+        },
+        searchIcon: {
+            width: '34px',
+            height: '34px',
+            left: '117px',
+            top: '147px',
+            position: 'absolute',
+            zIndex: 2
+        },
+        searchText: {
+            color: '#000000',
+            fontFamily: 'Lexend-Regular, Helvetica',
+            fontSize: '20px',
+            fontWeight: 300,
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            opacity: 0.5,
+            top: '152px',
+            left: '185px'
+        },
+        projectGridContainer: {
+            backgroundColor: '#f8f8f8',
+            width: '1219px',
+            height: '689px',
+            left: '93px',
+            top: '201px',
+            right: '607px',
+            position: 'absolute',
+            zIndex: 2,
+            border: '2px solid #000'
+        },
+        columnHeader: {
+            color: '#000000',
+            fontFamily: 'Lexend-Regular, Helvetica',
+            fontSize: '20px',
+            fontWeight: 300,
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            top: '14px',
+            left: '31px',
+            zIndex: 4
+        },
+        columnHeader1: {
+            color: '#000000',
+            fontFamily: 'Lexend-Regular, Helvetica',
+            fontSize: '20px',
+            fontWeight: 300,
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            top: '14px',
+            left: '173px',
+            zIndex: 4
+        },
+        columnHeader2: {
+            color: '#000000',
+            fontFamily: 'Lexend-Regular, Helvetica',
+            fontSize: '20px',
+            fontWeight: 300,
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            top: '14px',
+            right: '57px',
+            zIndex: 4
+        },
+        statusColumn: {
+            color: '#000000',
+            fontFamily: 'Lexend-Regular, Helvetica',
+            fontSize: '20px',
+            fontWeight: 300,
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            top: '14px',
+            left: '173px',
+            zIndex: 4
+        },
+        filterIcon1: {
+            left: '235px',
+            top: '20px',
+            width: '19.743px',
+            height: '9.636px',
+            zIndex: 4,
+            position: 'absolute'
+        },
+        titleColumn: {
+            color: '#000000',
+            fontFamily: 'Lexend-Regular, Helvetica',
+            fontSize: '20px',
+            fontWeight: 300,
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            top: '14px',
+            right: '57px',
+            zIndex: 4
+        },
+        filterIcon2: {
+            right: '30px',
+            top: '20px',
+            width: '19.743px',
+            height: '9.636px',
+            zIndex: 4,
+            position: 'absolute'
+        },
+        statusRow: {
+            backgroundColor: '#5E7190',
+            left: '5px',
+            right: '5px',
+            top: '46px',
+            width: '1205px',
+            height: '59px',
+            zIndex: 4,
+            position: 'absolute'
+        },
+        statusText: {
+            color: '#ffffff',
+            fontFamily: 'Lexend-Regular, Helvetica',
+            fontSize: '20px',
+            fontWeight: 300,
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            top: '17px',
+            left: '27px'
+        },
+        titleText: {
+            color: '#ffffff',
+            fontFamily: 'Lexend-Regular, Helvetica',
+            fontSize: '20px',
+            fontWeight: 300,
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            top: '17px',
+            left: '505px'
+        },
+        datePostedText: {
+            color: '#ffffff',
+            fontFamily: 'Lexend-Regular, Helvetica',
+            fontSize: '20px',
+            fontWeight: 300,
+            letterSpacing: 0,
+            lineHeight: 'normal',
+            position: 'absolute',
+            textAlign: 'center',
+            top: '17px',
+            right: '65px'
+        },
+        dividerVertical: {
+            backgroundColor: '#000000',
+            width: '2px',
+            height: '688px',
+            left: '149px',
+            position: 'absolute',
+            zIndex: 5
+        },
+        dividerHorizontal: {
+            backgroundColor: '#000000',
+            width: '1203px',
+            left: '5px',
+            right: '5px',
+            height: '2px',
+            top: '46px',
+            position: 'absolute',
+            zIndex: 6
+        }
+    };
+
     return (
-        <div class="projects-archived">
-            <div class="overlap-wrapper">
-                <div class="div-1">
-                    <div class="text-wrapper-1">Welcome User</div>
-                    <div class="p">S E R A P H</div>
-                    <img class="image" alt="Image" src={seraph_logo}/>
-                    <div class="text-wrapper-2">Home</div>
-                    <div class="text-wrapper-3">Archived</div>
-                </div>
-                    
-                <div class="div-2">
-                    <div class="text-wrapper-4">Your Past Projects</div>
-                    <img class="image2" alt="Image" src={seraph_logo}/>
-                    <div class="rectangle-1">
-                </div>
-                
-                <div class="export-pdf">Export Project PDF</div>
-                <div class="search_bar"></div>
-                <img class="search_icon" alt="Image" src={search_icon}/>
-                <div class="search-text">Search</div>
-                <div class="rectangle-2">
-                </div>
-                
-                <div class="div-3">
-                    <div class="text-wrapper-5">STATUS</div>
-                    <div class="text-wrapper-6">TITLE</div>
-                    <img class="filter-1" alt="Filter" src={filter_icon}/>
-                    <div class="text-wrapper-7">DATE POSTED</div>
-                    <img class="filter-2" alt="Filter" src={filter_icon}/>
-                    <div class="line-1"></div>
-                    <div class="line-2"></div>
-                    
-                    <div class="frame">
-                        <div class="text-wrapper-8">Complete</div>
-                        <div class="text-wrapper-9">MEDUSA</div>
-                        <div class="text-wrapper-10">Jun 10, 2024</div>
+        <div style={styles.mainContainer}>
+            <div style={styles.innerContainer}>
+
+                <div style={styles.projectListContainer}>
+                    <div style={styles.sectionTitle}>Your Past Projects</div>
+                    <img style={styles.projectImage} alt="Project Image" src={seraph_logo}/>
+
+                    <button className="button-box" style={styles.exportButton} onClick={() => window.location.href = '/pdfoverview'}> Export Project PDF </button>
+
+                    <img style={styles.searchIcon} alt="Search Icon" src={search_icon}/>
+
+                    <input type="text" style={styles.searchContainer} placeholder="Search " className="input-box"/>
+
+                    <div style={styles.projectGridContainer}>
+                        <div style={styles.columnHeader}>STATUS</div>
+                        <div style={styles.columnHeader1}>TITLE</div>
+                        <img style={styles.filterIcon1} alt="Filter Icon" src={filter_icon}/>
+                        <div style={styles.columnHeader2}>DATE POSTED</div>
+                        <img style={styles.filterIcon2} alt="Filter Icon" src={filter_icon}/>
+                        <div style={styles.statusRow}>
+                            <div style={styles.statusText}>Complete</div>
+                            <div style={styles.titleText}>MEDUSA</div>
+                            <div style={styles.datePostedText}>Jun 10, 2024</div>
+                        </div>
+                        <div style={styles.dividerVertical}></div>
+                        <div style={styles.dividerHorizontal}></div>
                     </div>
                 </div>
-                        </div>
             </div>
         </div>
     );
