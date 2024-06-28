b9938a23cfb36dd14be9e962a12165c09fdc7c38
Jason <tpajason123@gmail.com>
2024-06-28 12:37:17 -0400

    fixed models

--- backend/models.py
+++ backend/models.py
@@ -70,7 +70,7 @@ class Milestones(db.Model):
         }
 
 class Sprint(db.Model):
-    sprintid = db.Column(db.String(20), primary_key=True)
+    sprintid = db.Column(db.Integer, primary_key=True)
     sprintname = db.Column(db.String(20))
     projectid = db.Column(db.String(100), db.ForeignKey('project.projectid'))
     startdate = db.Column(db.Date)
