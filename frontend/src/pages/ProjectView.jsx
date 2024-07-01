949f8b1fa4678dff69fdd7e85b1f6bdaef455a3f
RayX52 <127812971+RayX52@users.noreply.github.com>
2024-07-01 10:43:58 -0400

    implemented GET functionality for projectView page

--- frontend/src/pages/ProjectView.jsx
+++ frontend/src/pages/ProjectView.jsx
@@ -1,20 +1,45 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './ProjectView.css';
+import { useState, useEffect } from 'react';
+
+import seraph_logo from '../assets/seraph_logo.png';
+import { useParams } from 'react-router-dom';
 
 function ProjectView() {
+    const { projectID } = useParams(); // get project id from the URL
+    const [project, setProject] = useState([])
+
+    useEffect(() => {
+        const fetchProject = async () => {
+            try {
+                const response = await fetch("http://127.0.0.1:5000/get_project/${projectID}")
+                if (!response.ok) {
+                    throw new Error("network response not valid"), 400
+                }
+                const data = await response.json()
+                setProject(data.project)
+                console.log(data.project)
+            } catch (error) {
+                console.error("Error fetching project data", error)
+            }
+        };
+
+        fetchProject();
+    }, []);
+
     return (
         <>
             <div className="parent">
                 <div className='box1'>
                     <div className="project-info">
-                        <img src="medusa.png" alt="Medusa Logo" className="project-logo" />
-                        <h1 className="project-title">MEDUSA</h1>
-                        <p>Team Members:</p>
-                        <p>Customer:</p>
-                        <p>Technologies Used:</p>
-                        <p>Description:</p>
+                        <img className="seraph_logo" alt="Seraph Logo" src={seraph_logo} />
+                        <h1 className="project-title">{project.projectName}</h1>
+                        <p>Team Members: {project.teamMembers ? project.teamMembers.join(', ') : "To be added"}</p>
+                        <p>Customer: {project.customerName}</p>
+                        <p>Technologies Used: {project.techUsed}</p>
+                        <p>Description: {project.description ? project.description : "To be added"}</p>
                         <div className="progress-bar-PV">
-                            <div className="progress-PV"></div>
+                            <div className="progress-PV" style={project.progress ? { width: `${project.progress}%` } : {width: `${project.progress}%` }}></div>
                         </div>
                     </div>
                 </div>
